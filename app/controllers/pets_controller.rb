cfd90e45454a5d1a41a7beb62df7835824c125cf
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-09 11:59:13 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,6 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pets])
-    binding.pry
     if !params[:owner][:name].empty?
       @pet.owner = Owner.create(name: params[:owner][:name])
     end
